83f518ea25a3c006182ad764981b7721bf870379
ghandic <andrewchallis@hotmail.co.uk>
2024-08-30 21:55:22 +1000

    fix reactive output

--- src/components/ccs-form.tsx
+++ src/components/ccs-form.tsx
@@ -69,6 +69,9 @@ const WhatIfAnalysisForm = ({
       form.setFieldValue(name, value, { dontUpdateMeta: true });
     });
   }, [defaultValues, form]);
+  useEffect(() => {
+    setResult(calculateTotalTakeHomePayWithChildcare(formState));
+  }, [formState]);
   return (
     <div className="p-4 space-y-4 w-full flex flex-col gap-4">
       <form
